9151c668561ca127786b778edd767a32efeb0abf
galuhnurdianty <32261792+galuhnurdianty@users.noreply.github.com>
2018-01-03 21:21:41 +0800

    input matrik X

--- hairunArraydenganinputan.cpp
+++ hairunArraydenganinputan.cpp
@@ -14,5 +14,24 @@ cout<<"\t\t\tNPM : 16630286\n";
 
 cout<<"\t\t\tPROGRAM : Teknik Informtika\n";
 cout<<"\t\t\tTeknik : INPUTAN ARRAY DIMENSI 2\n";
+  cout<<"Silahkan input matrik X : \n";
+ 
+   cout<<"------------------------- \n";
+ 
+   for(i=0;i<3;i++)
+ 
+   {
+ 
+        for(j=0;j<5;j++)
+ 
+        {
+ 
+        cout<<"matrik X "<<(i+1)<<","<<(j+1)<<" : ";
+ 
+      cin>>X[i][j];
+ 
+        }
+ 
+   }
  
 }
