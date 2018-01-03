c40db631467bc187561178fa8dd147d5f8fea2d1
galuhnurdianty <32261792+galuhnurdianty@users.noreply.github.com>
2018-01-03 21:20:52 +0800

    pembagian matrik int

--- hairunArraydenganinputan.cpp
+++ hairunArraydenganinputan.cpp
@@ -6,4 +6,13 @@ using namespace std;
 main()
  
 {
+	 int X[3][5],R[3][5],W[1][5],C[3][5],i,j,k;
+ 
+ cout<<"\n\t\t\tNAMA : Khairunnaji\n";
+
+cout<<"\t\t\tNPM : 16630286\n";
+
+cout<<"\t\t\tPROGRAM : Teknik Informtika\n";
+cout<<"\t\t\tTeknik : INPUTAN ARRAY DIMENSI 2\n";
+ 
 }
